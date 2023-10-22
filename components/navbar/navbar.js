dbd344eb9475253edea5d93d4d1dbc585fe9b50d
Olvin Paz <olvinpaz@gmail.com>
2023-10-22 10:24:36 -0700

    Updated Router

--- components/navbar/navbar.js
+++ components/navbar/navbar.js
@@ -1,15 +1,18 @@
-const btnHome = document.getElementById('btnHome');
-const btnRepeatedNumbers = document.getElementById('btnRN');
-const btnSelectedNumbers = document.getElementById('btnSN');
+
 
 // Cuando el documento se carga completamente, se ejecutará este código.
 window.addEventListener('load', function () {
-    var currentURL = window.location.href;
-    console.log("URL actual: " + currentURL);
+    //var currentURL = window.location.href;
+    //console.log("URL actual: " + currentURL);
+
+    const btnHome = document.getElementById('btnHome');
+    const btnRepeatedNumbers = document.getElementById('btnRN');
+    const btnSelectedNumbers = document.getElementById('btnSN');
 
     // Verificar si estás en una página específica:
-    if (window.location.href.includes("index.html")) {
-        console.log("Estás en la página 'index.html'");
+
+    if (window.location.href.includes("index.html") || window.location.href.includes("/")) {
+        //console.log("Estás en la página 'index.html'");
 
         btnHome.classList.add("selected");
         btnRepeatedNumbers.classList.remove("selected");
@@ -17,7 +20,7 @@ window.addEventListener('load', function () {
     }
 
     if (window.location.href.includes("repeated/numbers.html")) {
-        console.log("Estás en la página 'numbers.html'");
+        //console.log("Estás en la página 'numbers.html'");
 
         btnHome.classList.remove("selected");
         btnRepeatedNumbers.classList.add("selected");
@@ -25,7 +28,7 @@ window.addEventListener('load', function () {
     }
 
     if (window.location.href.includes("selected/numbers.html")) {
-        console.log("Estás en la página 'selected/numbers.html'");
+        //console.log("Estás en la página 'selected/numbers.html'");
 
         btnHome.classList.remove("selected");
         btnRepeatedNumbers.classList.remove("selected");
